846f27d0a264f181b30ff181d2f7a524b14ed8f5
SonofaPancak <58006178+SonofaPancak@users.noreply.github.com>
2022-05-26 11:41:48 -0400

    Ajout du batchfile de création dev-env
    (Fonctionne sur window 10 sur ma machine) On doit spécifier le path d'installation et ça créer "bdsol-workspace" contenant l'environnement workspace, et deux sous-fichier contenant chacun les projet "app" et "api".

---
+++ deploy-dev-env.bat
@@ -0,0 +1,48 @@
+REM Demande l'endroit d'installation
+
+@ECHO OFF
+CLS
+SET /p R="Repertoire de base ( ex: C:/Subversion/src ) :"
+
+ECHO Set les variables path
+
+ECHO Aller au chemin de base
+cd %R%
+
+
+ECHO Clone le git data
+git clone https://github.com/Avantage-Numerique/bdsol-workspace.git
+CD bdsol-workspace
+RD /S /Q .git
+
+ECHO Clone le git api
+git clone https://github.com/Avantage-Numerique/bdsol-api.git
+REN bdsol-api api
+CD api
+COPY .env.exemple .env
+
+REM Vérifie Dockerfile.dev
+CALL npm install --force
+CD ..
+
+ECHO Clone le git app
+git clone https://github.com/Avantage-Numerique/bdsol-app.git
+REN bdsol-app app
+CD app
+COPY .env.exemple .env
+
+REM Vérifie Dockerfile.dev
+
+CALL npm install --force
+CD ..
+
+
+ECHO Build le docker
+CALL docker compose build
+
+REM Start a cmd et run frontend
+START "Environnement Frontend" cmd.exe /K "CD app && npm run dev"
+
+REM Run api
+CD api
+docker compose up
